fc13a4ef258a69e3e1480c4cc7e7fe964c0d33d7
jonathan.philip@gmail.com <jonathan.philip@gmail.com>
2018-12-02 17:56:57 +1100

    More todoReducers tests

--- src/store/reducers/todoReducers.test.js
+++ src/store/reducers/todoReducers.test.js
@@ -2,7 +2,7 @@ import todoReducer from './todoReducers';
 import { ADD_TODO } from '../actions/types';
 
 describe('todoReducers', () => {
-    it('should add new item to list with incremented ID', () => {
+    it('should add item to empty list', () => {
         const initialState = {
             todos: [],
             todo: {}
@@ -23,5 +23,30 @@ describe('todoReducers', () => {
         const state = todoReducer(initialState, action);
 
         expect(state.todo).toEqual(expectedTodo);
+        expect(state.todos).toEqual([expectedTodo]);
+    });
+
+    it('should add another item to non-empty list and increment ID', () => {
+        const initialState = {
+            todos: [{ id: 1 }],
+            todo: {}
+        };
+
+        const action = {
+            type: ADD_TODO,
+            payload: 'title'
+        }
+
+        const expectedTodo = {
+            userId: 2,
+            id: 2,
+            title: action.payload,
+            completed: true
+        };
+
+        const state = todoReducer(initialState, action);
+
+        expect(state.todo).toEqual(expectedTodo);
+        expect(state.todos).toEqual([...initialState.todos, expectedTodo]);
     });
 });
